c1e8b5da5702345dcfe71e1909d69fa076cb3d4c
Mikhail <mam.mih.val@gmail.com>
2023-03-19 19:55:02 +0300

    Adopting for NICA

--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-#SBATCH -p fast
-#SBATCH -t 01:00:00
-#SBATCH -J bmn_converter
-#SBATCH -o /home/ovgol/log/%A_%a.log
+#$ -wd /scratch1/mmamaev/data/DCM-QGSM-SMM/3.0
+#$ -cwd
+#$ -N Convert
+#$ -q all.q
+#$ -l h=!(ncx182.jinr.ru|ncx211.jinr.ru)
+#$ -l h_rt=01:00:00
+#$ -l s_rt=01:00:00
+#
+#$ -o /scratch1/mmamaev/data/DCM-QGSM-SMM/3.0/log
+#$ -e /scratch1/mmamaev/data/DCM-QGSM-SMM/3.0/log
 
 in=$1
 shft=$2
 [ -z $shft ] && shft=0
 id=$SLURM_ARRAY_TASK_ID
-out=/mnt/pool/nica/7/ovgol/mc/bmnsim/XeCs
+out=/scratch1/mmamaev/data/DCM-QGSM-SMM/3.0
 mkdir -pv $out
 mkdir -pv $out/qa
 
-. /mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/build/config.sh
+. /scratch1/mmamaev/bmn_environment.sh
 
 mkdir $out
-time root -b -l -q /home/ovgol/soft/bmn_converter/convertBmn.C"(\"${in}/${id}/dst2_geant_output.root\", \"${in}/${id}/geant_output.root\", \"${out}/$((${id}+${shft})).tree.root\")"
-time root -b -l -q /home/ovgol/soft/bmn_converter/qa.C"(\"${out}/$((${id}+${shft})).tree.root\", \"${out}/qa/$((${id}+${shft})).qa.root\")"
+time root -b -l -q /scratch1/mmamaev/bmnConverter/convertBmn.C"(\"${in}/${id}/dst2_geant_output.root\", \"${in}/${id}/geant_output.root\", \"${out}/$((${id}+${shft})).tree.root\")"
+time root -b -l -q /scratch1/mmamaev/bmnConverter/qa.C"(\"${out}/$((${id}+${shft})).tree.root\", \"${out}/qa/$((${id}+${shft})).qa.root\")"
