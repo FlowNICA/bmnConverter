29475d00749da4ebed454ceba355dcf251132b29
Mikhail <mam.mih.val@gmail.com>
2023-03-23 18:07:22 +0300

    Updating the batch macros

--- run.sh
+++ run.sh
@@ -8,21 +8,19 @@
 #$ -l h_rt=01:00:00
 #$ -l s_rt=01:00:00
 #
-#$ -o /scratch1/mmamaev/data/DCM-QGSM-SMM/3.0/log
-#$ -e /scratch1/mmamaev/data/DCM-QGSM-SMM/3.0/log
+#$ -o /scratch1/mmamaev/data/log
+#$ -e /scratch1/mmamaev/data/log
 
 in=$1
-shft=$2
-[ -z $shft ] && shft=0
+out=$2
 id=$SGE_TASK_ID
-out=/scratch1/mmamaev/data/DCM-QGSM-SMM/3.0
+
 mkdir -pv $out
-mkdir -pv $out/qa
+#mkdir -pv $out/qa
 
 source /cvmfs/nica.jinr.ru/sw/os/login.sh
 module add GCC-Toolchain/
 source /scratch1/mmamaev/bmn_environment.sh
 
-mkdir $out
-time root -b -l -q /scratch1/mmamaev/bmnConverter/convertBmn.C"(\"${in}/${id}/dst_geant_output.root\", \"${in}/${id}/geant_output.root\", \"${in}/${id}/full_geometry.root\", \"${out}/$((${id}+${shft})).tree.root\")"
-time root -b -l -q /scratch1/mmamaev/bmnConverter/qa.C"(\"${out}/$((${id}+${shft})).tree.root\", \"${out}/qa/$((${id}+${shft})).qa.root\")"
+time root -b -l -q /scratch1/mmamaev/bmnConverter/convertBmn.C"(\"${in}/${id}/dst_geant_output.root\", \"${in}/${id}/geant_output.root\", \"${in}/${id}/full_geometry .root\", \"${out}/${id}.tree.root\")"
+#time root -b -l -q /scratch1/mmamaev/bmnConverter/qa.C"(\"${out}/${id}.tree.root\", \"${out}/qa/${id}.qa.root\")"
